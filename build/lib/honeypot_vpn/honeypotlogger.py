5bf86eb50459235c8c7ffe5cdb401a03b7209f3b
ivan3129 <ivan31299@hotmail.com>
2022-09-05 00:58:21 -0500

    Wait to connect python with logstash

--- build/lib/honeypot_vpn/honeypotlogger.py
+++ build/lib/honeypot_vpn/honeypotlogger.py
@@ -9,6 +9,7 @@ import platform
 import os
 import socket
 import time
+import threading
 
 class HoneypotLogger:
 
@@ -19,20 +20,33 @@ class HoneypotLogger:
     def wait_for_logstash_connection(self,ip,port):
         maxattempts = 60
         attempt = 0
+        logger = logging.getLogger('HoneypotVPNLogger')
+        logger.setLevel(logging.DEBUG)
         while True:
             try:
                 attempt = attempt + 1
+                if attempt > maxattempts:
+                    logger.info("Can't connect to Logstash at " + ip +":"+ str(port) + " -> Max attempts reached:" + str(maxattempts))
+                    break
                 time.sleep(5)
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 socket.setdefaulttimeout(5.0)
                 result = sock.connect_ex((ip,port))
                 if result == 0:
-                    print("Logstash connected to " + ip +":"+ str(port) + " -> Attempt:" + str(attempt))
-                    return
-                print("Waiting for logstash service up at " + ip +":"+ str(port) + " -> Attempt:" + str(attempt))
+                    logger.info("Logstash connected to " + ip +":"+ str(port) + " -> Attempt:" + str(attempt))
+                    break
+                logger.info("Waiting for logstash service up at " + ip +":"+ str(port) + " -> Attempt:" + str(attempt))
             except socket.error as e:
-                print("Error")
+                logger.info(e)
                 pass
+        logger.addHandler(logstash.TCPLogstashHandler(ip, port, version=1))
+        extra = {
+            'elastic_fields': {
+            'version': 'python version: ' + repr(sys.version_info)
+            }
+        }
+        logger.info("Logger was configured", extra=extra)
+        
 
     def configureLogger(self, writeFileLogs = True, sendToElasticSearch = False,  logstashHost = 'localhost:5000', folderLogs="/honeypotvpn/logs"):
     	
@@ -41,26 +55,21 @@ class HoneypotLogger:
             os.makedirs(folderLogs)
         logger = logging.getLogger('HoneypotVPNLogger')
         logger.setLevel(logging.DEBUG)
-        if sendToElasticSearch:
-            logstashHost = logstashHost.replace("http://","")
-            logstashHost = logstashHost.replace("https://","")
-            logstashconf = logstashHost.split(":")
-            self.wait_for_logstash_connection(logstashconf[0], int(logstashconf[1]))
-            logger.addHandler(logstash.TCPLogstashHandler(logstashconf[0], int(logstashconf[1]), version=1))
-            extra = {
-                'elastic_fields': {
-                    'version': 'python version: ' + repr(sys.version_info)
-                }
-            }
-            logger.info("Logger was configured", extra=extra)
-        else:
-            logging.config.fileConfig(log_file_path)
         if writeFileLogs: 
             fh = logging.FileHandler(folderLogs+ '/{:%Y-%m-%d}.log'.format(datetime.now()), mode='a')
             formatter = logging.Formatter('%(asctime)s | %(levelname)-8s | %(lineno)04d | %(message)s')
             fh.setFormatter(formatter)
             logger.addHandler(fh)
             logger.info('WRITING LOG FILE:' + folderLogs + '/{:%Y-%m-%d}.log'.format(datetime.now()))
+        if sendToElasticSearch:
+            logstashHost = logstashHost.replace("http://","")
+            logstashHost = logstashHost.replace("https://","")
+            logstashconf = logstashHost.split(":")
+            threading.Thread(target=self.wait_for_logstash_connection, args=(logstashconf[0],int(logstashconf[1]),)).start()
+            
+        else:
+            logging.config.fileConfig(log_file_path)
+
 
         print("configuredddddd...")
 
--- honeypot_vpn/honeypotlogger.py
+++ honeypot_vpn/honeypotlogger.py
@@ -8,6 +8,8 @@ import logstash
 import platform
 import os
 import socket
+import time
+import threading
 
 class HoneypotLogger:
 
@@ -16,7 +18,7 @@ class HoneypotLogger:
         
     
     def wait_for_logstash_connection(self,ip,port):
-        maxattempts = 60
+        maxattempts = 80
         attempt = 0
         logger = logging.getLogger('HoneypotVPNLogger')
         logger.setLevel(logging.DEBUG)
@@ -25,18 +27,26 @@ class HoneypotLogger:
                 attempt = attempt + 1
                 if attempt > maxattempts:
                     logger.info("Can't connect to Logstash at " + ip +":"+ str(port) + " -> Max attempts reached:" + str(maxattempts))
-                    return
+                    break
+                time.sleep(5)
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 socket.setdefaulttimeout(5.0)
                 result = sock.connect_ex((ip,port))
                 if result == 0:
                     logger.info("Logstash connected to " + ip +":"+ str(port) + " -> Attempt:" + str(attempt))
-                    return
+                    break
                 logger.info("Waiting for logstash service up at " + ip +":"+ str(port) + " -> Attempt:" + str(attempt))
-                print("Waiting for logstash service up at " + ip +":"+ str(port) + " -> Attempt:" + str(attempt))
             except socket.error as e:
                 logger.info(e)
                 pass
+        logger.addHandler(logstash.TCPLogstashHandler(ip, port, version=1))
+        extra = {
+            'elastic_fields': {
+            'version': 'python version: ' + repr(sys.version_info)
+            }
+        }
+        logger.info("Logger was configured", extra=extra)
+        
 
     def configureLogger(self, writeFileLogs = True, sendToElasticSearch = False,  logstashHost = 'localhost:5000', folderLogs="/honeypotvpn/logs"):
     	
@@ -55,14 +65,8 @@ class HoneypotLogger:
             logstashHost = logstashHost.replace("http://","")
             logstashHost = logstashHost.replace("https://","")
             logstashconf = logstashHost.split(":")
-            self.wait_for_logstash_connection(logstashconf[0], int(logstashconf[1]))
-            logger.addHandler(logstash.TCPLogstashHandler(logstashconf[0], int(logstashconf[1]), version=1))
-            extra = {
-                'elastic_fields': {
-                    'version': 'python version: ' + repr(sys.version_info)
-                }
-            }
-            logger.info("Logger was configured", extra=extra)
+            threading.Thread(target=self.wait_for_logstash_connection, args=(logstashconf[0],int(logstashconf[1]),)).start()
+            
         else:
             logging.config.fileConfig(log_file_path)
 
