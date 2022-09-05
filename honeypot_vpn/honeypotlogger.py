dbfbf3340d53b3e175cb4e8dd6e867d12c007be6
ivan3129 <ivan31299@hotmail.com>
2022-09-05 00:09:05 -0500

    Wait to connect python with logstash

--- honeypot_vpn/honeypotlogger.py
+++ honeypot_vpn/honeypotlogger.py
@@ -19,21 +19,24 @@ class HoneypotLogger:
     def wait_for_logstash_connection(self,ip,port):
         maxattempts = 60
         attempt = 0
+        logger = logging.getLogger('HoneypotVPNLogger')
+        logger.setLevel(logging.DEBUG)
         while True:
             try:
                 attempt = attempt + 1
                 if attempt > maxattempts:
-                    print("Can't connect to Logstash at " + ip +":"+ str(port) + " -> Max attempts reached:" + str(maxattempts))
+                    logger.info("Can't connect to Logstash at " + ip +":"+ str(port) + " -> Max attempts reached:" + str(maxattempts))
                     return
                 time.sleep(5)
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 socket.setdefaulttimeout(5.0)
                 result = sock.connect_ex((ip,port))
                 if result == 0:
-                    print("Logstash connected to " + ip +":"+ str(port) + " -> Attempt:" + str(attempt))
+                    logger.info("Logstash connected to " + ip +":"+ str(port) + " -> Attempt:" + str(attempt))
                     return
-                print("Waiting for logstash service up at " + ip +":"+ str(port) + " -> Attempt:" + str(attempt))
+                logger.info("Waiting for logstash service up at " + ip +":"+ str(port) + " -> Attempt:" + str(attempt))
             except socket.error as e:
+                logger.info(e)
                 pass
 
     def configureLogger(self, writeFileLogs = True, sendToElasticSearch = False,  logstashHost = 'localhost:5000', folderLogs="/honeypotvpn/logs"):
@@ -43,6 +46,12 @@ class HoneypotLogger:
             os.makedirs(folderLogs)
         logger = logging.getLogger('HoneypotVPNLogger')
         logger.setLevel(logging.DEBUG)
+        if writeFileLogs: 
+            fh = logging.FileHandler(folderLogs+ '/{:%Y-%m-%d}.log'.format(datetime.now()), mode='a')
+            formatter = logging.Formatter('%(asctime)s | %(levelname)-8s | %(lineno)04d | %(message)s')
+            fh.setFormatter(formatter)
+            logger.addHandler(fh)
+            logger.info('WRITING LOG FILE:' + folderLogs + '/{:%Y-%m-%d}.log'.format(datetime.now()))
         if sendToElasticSearch:
             logstashHost = logstashHost.replace("http://","")
             logstashHost = logstashHost.replace("https://","")
@@ -57,12 +66,7 @@ class HoneypotLogger:
             logger.info("Logger was configured", extra=extra)
         else:
             logging.config.fileConfig(log_file_path)
-        if writeFileLogs: 
-            fh = logging.FileHandler(folderLogs+ '/{:%Y-%m-%d}.log'.format(datetime.now()), mode='a')
-            formatter = logging.Formatter('%(asctime)s | %(levelname)-8s | %(lineno)04d | %(message)s')
-            fh.setFormatter(formatter)
-            logger.addHandler(fh)
-            logger.info('WRITING LOG FILE:' + folderLogs + '/{:%Y-%m-%d}.log'.format(datetime.now()))
+
 
         print("configuredddddd...")
 
